5ef8e0bd7c676c8f47d56777b9fa03cc2422f552
floatingbug <sieber.thomas@yahoo.com>
2024-10-01 22:11:15 +0200

    added validateItems in directory middleware

--- src/middleware/validateItems.js
+++ src/middleware/validateItems.js
@@ -1,15 +1,26 @@
 function validateItems(req, res, next){
 	if(!req.body || Object.keys(req.body).length > 2 ||
 		!req.body.type || !req.body.items ||
-		!Array.isArray(req.body.items) || 
+		!Array.isArray(req.body.items) ||
 		req.body.items.length === 0 ||
-		req.body.items.length > 100)
+		req.body.items.length > 200)
 	{
-		res.status(404).res.json({success: false, msg: "Invalid properties."}
+		res.status(404).json({success: false, msg: "Invalid properties."});
 		return;
 	}
 
+	const isValidItems = req.body.items.every(item => {
+		if(typeof item !== "object") return false;
+	});
 
+	if(!isValidItems){
+		res.status(404)
+		res.json({success: false, msg: "Invalid properties in items."});
+		return;
+	}
 
 	next();
 }
+
+
+module.exports = {validateItems};
